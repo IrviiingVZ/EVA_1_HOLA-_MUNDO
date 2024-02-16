c554fb6e361a1114ca63127e090dbbec99d907e5
IrviiingVZ <150636449+IrviiingVZ@users.noreply.github.com>
2024-02-15 23:13:55 -0600

    Add files via upload

---
+++ EVA1_4_/src/main/java/com/mycompany/eva1/Eva1.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1;
+
+/**
+ *
+ * @author irvingvalles
+ */
+public class Eva1 {
+
+    public static void main(String[] args) {
+        int edad;//DECLARACIÔN
+        edad = 20 ; //ASIGNACIÔN --> UN SOLO = SIGNIFICA ASIGNAR 
+            System.out.println("Tu edad es:");
+            System.out.println(edad);
+        
+        String nombre;
+        nombre = "Irving Yael Valles Zuñiga";
+            System.out.println("Nombre del estudiante:");
+            System.out.println(nombre);
+        
+        String carrera;
+        carrera = "Ing en sistemas computacionales";
+            System.out.println("Su carrera es");
+            System.out.println(carrera);
+
+    }
+}
