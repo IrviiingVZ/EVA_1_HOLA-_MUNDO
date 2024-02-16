4ccf729735419bafc701e8ab88b114c5ce51f424
IrviiingVZ <150636449+IrviiingVZ@users.noreply.github.com>
2024-02-15 22:11:55 -0600

    Add files via upload

---
+++ EVA_1_1_HOLA-MUNDO/src/main/java/com/mycompany/eva_1_1_hola/mundo/EVA_1_1_HOLAMUNDO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva_1_1_hola.mundo;
+
+/**
+ *
+ * @author irvingvalles
+ */
+public class EVA_1_1_HOLAMUNDO {
+
+    public static void main(String[] args) {
+        System.out.println("HOLA MUNDO!");
+    }
+}
